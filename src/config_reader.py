e5c89e8586eece4734a83df77a28701be6a1cd93
Fsoky <cyberuest0x12@gmail.com>
2024-02-13 00:00:51 +0600

    config

---
+++ src/config_reader.py
@@ -0,0 +1,19 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+ENV_FILE_DIR = os.path.abspath(".")
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: SecretStr
+    DATABASE_URL: SecretStr
+    REDIS_URL: str = "redis://localhost:6379"
+
+    model_config = SettingsConfigDict(
+        env_file=f"{ENV_FILE_DIR}/.env",
+        env_file_encoding="utf-8"
+    )
+
+
+config = Settings()
